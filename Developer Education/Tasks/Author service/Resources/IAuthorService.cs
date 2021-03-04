d4fd3fb0aed32f9e6d1c58c96b82e215be02706f
Mårten Wahlund <marten.wahlund@litium.com>
2021-03-04 17:04:50 +0100

    Add RequireServiceImplementation to Author service task
    Fixes #12

--- Developer Education/Tasks/Author service/Resources/IAuthorService.cs
+++ Developer Education/Tasks/Author service/Resources/IAuthorService.cs
@@ -4,9 +4,11 @@ using Litium.Runtime.DependencyInjection;
 
 namespace Litium.Accelerator.Services
 {
-	[Service(ServiceType = typeof(IAuthorService), Lifetime = DependencyLifetime.Transient)]
-	public interface IAuthorService
-	{
-		List<string> GetBooksByAuthor(Guid authorPageId);
-	}
+    [Service(ServiceType = typeof(IAuthorService), Lifetime = DependencyLifetime.Transient)]
+    // Add RequireServiceImplementation to not allow Litium to start without an implementation of this interface:
+    [RequireServiceImplementation]
+    public interface IAuthorService
+    {
+        List<string> GetBooksByAuthor(Guid authorPageId);
+    }
 }
