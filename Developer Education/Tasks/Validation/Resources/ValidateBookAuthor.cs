4f48e0d2b462c7d2b4a44616ca32be754840f1f0
Mårten Wahlund <marten.wahlund@litium.com>
2021-04-07 15:52:43 +0200

    Adjusted validation sample to use nullable guid.
    #30

--- Developer Education/Tasks/Validation/Resources/ValidateBookAuthor.cs
+++ Developer Education/Tasks/Validation/Resources/ValidateBookAuthor.cs
@@ -6,41 +6,41 @@ using Litium.Websites;
 
 namespace Litium.Accelerator.ValidationRules
 {
-	public class ValidateBookAuthor : ValidationRuleBase<BaseProduct>
-	{
-		private readonly FieldTemplateService _fieldTemplateService;
-		private readonly PageService _pageService;
+    public class ValidateBookAuthor : ValidationRuleBase<BaseProduct>
+    {
+        private readonly FieldTemplateService _fieldTemplateService;
+        private readonly PageService _pageService;
 
-		public ValidateBookAuthor(PageService pageService, FieldTemplateService fieldTemplateService)
-		{
-			_pageService = pageService;
-			_fieldTemplateService = fieldTemplateService;
-		}
+        public ValidateBookAuthor(PageService pageService, FieldTemplateService fieldTemplateService)
+        {
+            _pageService = pageService;
+            _fieldTemplateService = fieldTemplateService;
+        }
 
-		public override ValidationResult Validate(BaseProduct entity, ValidationMode validationMode)
-		{
-			var authorPageId = entity.Fields.GetValue<Guid>("AuthorField");
+        public override ValidationResult Validate(BaseProduct entity, ValidationMode validationMode)
+        {
+            var authorPageId = entity.Fields.GetValue<Guid?>("AuthorField");
 
-			var result = new ValidationResult();
-			if (authorPageId == Guid.Empty)
-				return result;
+            var result = new ValidationResult();
+            if (authorPageId == null || authorPageId == Guid.Empty)
+                return result;
 
-			var authorPage = _pageService.Get(authorPageId);
-			if (authorPage == null)
-				return result;
+            var authorPage = _pageService.Get((Guid) authorPageId);
+            if (authorPage == null)
+                return result;
 
-			var pageFieldTemplate = _fieldTemplateService.Get<FieldTemplate>(authorPage.FieldTemplateSystemId);
-			var isAuthorTemplate = pageFieldTemplate.Id.Equals("Author");
-			if (!isAuthorTemplate)
-			{
-				// Pass ID of a field as first parameter to display the error next to that field in backoffice-UI
-				result.AddError("AuthorField", "Only Author-pages can be selected as author");
+            var pageFieldTemplate = _fieldTemplateService.Get<FieldTemplate>(authorPage.FieldTemplateSystemId);
+            var isAuthorTemplate = pageFieldTemplate.Id.Equals("Author");
+            if (!isAuthorTemplate)
+            {
+                // Pass ID of a field as first parameter to display the error next to that field in backoffice-UI
+                result.AddError("AuthorField", "Only Author-pages can be selected as author");
 
-				// ...and/or pass "*" to display the validation error in the header.
-				result.AddError("*", "Author page validation failed");
-			}
+                // ...and/or pass "*" to display the validation error in the header.
+                result.AddError("*", "Author page validation failed");
+            }
 
-			return result;
-		}
-	}
+            return result;
+        }
+    }
 }
