7406b6dc55fd9dec05ca07478c4df47337b2888a
Mårten Wahlund <marten.wahlund@litium.com>
2022-03-03 15:49:02 +0100

    Added comments explaining the code in validation task code sample
    Fixes #96

--- Developer Education/Tasks/Validation/Resources/ValidateBookAuthor.cs
+++ Developer Education/Tasks/Validation/Resources/ValidateBookAuthor.cs
@@ -4,43 +4,53 @@ using Litium.Products;
 using Litium.Validations;
 using Litium.Websites;
 
-namespace Litium.Accelerator.ValidationRules
+namespace Litium.Accelerator.ValidationRules;
+
+public class ValidateBookAuthor : ValidationRuleBase<BaseProduct>
 {
-    public class ValidateBookAuthor : ValidationRuleBase<BaseProduct>
+    private readonly FieldTemplateService _fieldTemplateService;
+    private readonly PageService _pageService;
+
+    public ValidateBookAuthor(PageService pageService, FieldTemplateService fieldTemplateService)
     {
-        private readonly FieldTemplateService _fieldTemplateService;
-        private readonly PageService _pageService;
+        _pageService = pageService;
+        _fieldTemplateService = fieldTemplateService;
+    }
 
-        public ValidateBookAuthor(PageService pageService, FieldTemplateService fieldTemplateService)
-        {
-            _pageService = pageService;
-            _fieldTemplateService = fieldTemplateService;
-        }
+    public override ValidationResult Validate(BaseProduct entity, ValidationMode validationMode)
+    {
+        var result = new ValidationResult();
 
-        public override ValidationResult Validate(BaseProduct entity, ValidationMode validationMode)
-        {
-            var authorPageId = entity.Fields.GetValue<Guid?>("AuthorField");
+        // Get id of the selected page
+        var authorPageId = entity.Fields.GetValue<Guid?>("AuthorField");
 
-            var result = new ValidationResult();
-            if (authorPageId == null || authorPageId == Guid.Empty)
-                return result;
-
-            var authorPage = _pageService.Get((Guid) authorPageId);
-            if (authorPage == null)
-                return result;
-
-            var pageFieldTemplate = _fieldTemplateService.Get<FieldTemplate>(authorPage.FieldTemplateSystemId);
-            var isAuthorTemplate = pageFieldTemplate.Id.Equals("Author");
-            if (!isAuthorTemplate)
-            {
-                // Pass ID of a field as first parameter to display the error next to that field in backoffice-UI
-                result.AddError("AuthorField", "Only Author-pages can be selected as author");
-
-                // ...and/or pass "*" to display the validation error in the header.
-                result.AddError("*", "Author page validation failed");
-            }
-
+        // No need to validate if a page has not been selected
+        if (authorPageId == null || authorPageId == Guid.Empty)
             return result;
-        }
+
+        // No need to validate if page cannot be found (perhaps deleted after property was set?)
+        var authorPage = _pageService.Get((Guid)authorPageId);
+        if (authorPage == null)
+            return result;
+
+        // A page is selected, get its template and make sure it is the 
+        // author-template created in the author page task
+        var pageFieldTemplate = _fieldTemplateService.Get<FieldTemplate>(authorPage.FieldTemplateSystemId);
+        var isAuthorTemplate = pageFieldTemplate.Id.Equals("Author");
+
+        // If author template is used then all is good
+        if (isAuthorTemplate)
+            return result;
+
+        // An invalid template is selected, add errors to the returned result
+        // The first parameter of the AddError-method is used to identify a field to attach
+        // the validation message to, pass the id of a field to display the error message
+        // next to that field in the UI:
+        result.AddError("AuthorField", "Only Author-pages can be selected as author");
+
+        // ...and/or pass "*" to display the validation error in the header.
+        result.AddError("*", "Author page validation failed");
+
+        return result;
     }
 }
